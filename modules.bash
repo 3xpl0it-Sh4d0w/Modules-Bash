21103d7eb284cf382b5cf6006ef32da9c6f3b86e
3xpl0it.Sh4d0w <3xpl0it.sh4d0w@pm.me>
2022-05-11 21:31:22 +0200

    Update modules.bash

--- modules.bash
+++ modules.bash
@@ -3,19 +3,18 @@
 # MODULES - By : 3xpl0it sh4d0w - © All Rights Reserved : 2022 
 # ============================================================
 # DATE DE CRÉATION      :   11-05-2022 05:11
-# DATE DE MODIFICATION  :   11-05-2022 20:10
+# DATE DE MODIFICATION  :   11-05-2022 21:31
 # ============================================================
+
+chmod +x ${DIR_MODULES}colors.bash
+. ${DIR_MODULES}colors.bash
+
+module.colors
 
 COMMAND="$1"
 
 DIR_MODULES="modules/"
 DIR_INSTALL="/opt/shadow.mods"
-
-chmod +x ${DIR_MODULES}colors.bash
-
-. ${DIR_MODULES}colors.bash
-
-module.colors
 
 if [[ "$COMMAND" = "-i" ]] || \
 [[ "$COMMAND" = "install" ]] || \
