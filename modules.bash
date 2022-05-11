b2d973da3fa17ce07ec5d35eb1e3b4b0beb3ed92
3xpl0it.Sh4d0w <3xpl0it.sh4d0w@pm.me>
2022-05-11 05:46:08 +0200

    Update modules.bash

--- modules.bash
+++ modules.bash
@@ -3,11 +3,30 @@
 # MODULES - By : 3xpl0it sh4d0w - © All Rights Reserved : 2022 
 # ============================================================
 # DATE DE CRÉATION      :   11-05-2022 05:11
-# DATE DE MODIFICATION  :   11-05-2022 05:11
+# DATE DE MODIFICATION  :   11-05-2022 05:46
 # ============================================================
 
+COMMAND="$1"
+
 DIR_MODULES="modules/"
+DIR_INSTALL="/opt/shadow.mods"
 
 chmod +x ${DIR_MODULES}colors.bash
 
 . ${DIR_MODULES}colors.bash
+
+module.colors
+
+if [[ "$COMMAND" = "-i" ]] \
+[[ "$COMMAND" = "install" ]] \
+[[ "$COMMAND" = "--install" ]]:
+  then
+    mkdir ${DIR_INSTALL}
+    cp modules.bash ${DIR_INSTALL}
+    cp ${DIR_MODULES}color.bash ${DIR_INSTALL}
+fi
+
+if [[ ! "$COMMAND" ]];
+  then
+    printf "${grey}[${none} ${red}ERREUR${none} ${grey}]${none} ${cyan}La commande ${1} n'existe pas !${none}\n"
+fi
